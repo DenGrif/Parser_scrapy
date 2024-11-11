423811c25cbf3b6a2814550cd9557cf269aca58d
DenGrif <grifon991@gmail.com>
2024-11-12 02:37:35 +0400

    05

--- 003.py
+++ 003.py
@@ -16,8 +16,8 @@ parsed_data = []
 for vacancy in vacancies:
     try:
         title = vacancy.find_element(By.CSS_SELECTOR, 'span.magritte-text___tkzIl_4-3-9').text
-        company = vacancy.find_element(By.CSS_SELECTOR, 'span.magritte-text___tkzIl_4-3-9').text
-        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.magritte-text___pbpft_3-0-16').text
+        company = vacancy.find_element(By.CSS_SELECTOR, 'span[data-qa="vacancy-serp__vacancy-employer-text"]').text
+        salary = vacancy.find_element(By.CSS_SELECTOR, 'div.pY3d2 span.magritte-text___pbpft_3-0-16').text
         link = vacancy.find_element(By.CSS_SELECTOR, 'a.magritte-link___b4rEM_4-3-9').get_attribute('href')
     except Exception as e:
         print(f"Произошла ошибка при парсинге: {e}")
@@ -28,9 +28,9 @@ for vacancy in vacancies:
 driver.quit()
 
 # Записываем данные в CSV с добавлением BOM для корректного отображения кириллицы в Excel и с разделителем ;
-with open("hh3.csv", 'w', newline='', encoding='utf-8-sig') as file:
+with open("hh05.csv", 'w', newline='', encoding='utf-8-sig') as file:
     writer = csv.writer(file, delimiter=';')  # Указываем разделитель ;
     writer.writerow(['Название вакансии', 'Название компании', 'Зарплата', 'Ссылка на вакансию'])
     writer.writerows(parsed_data)
 
-print("Парсинг завершен. Данные сохранены в hh3.csv")
+print("Парсинг завершен. Данные сохранены в hh05.csv")
